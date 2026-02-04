b4275850fa9debc9a684b5f921180b8c7ad6f166
developerlmp <74241307+developerlmp@users.noreply.github.com>
2026-02-04 22:40:11 +0700

    Update tasks.py
    Signed-off-by: developerlmp <74241307+developerlmp@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -132,7 +132,7 @@ class BaseModel(torch.nn.Module):
         Returns:
             (torch.Tensor): Loss if x is a dict (training), or network predictions (inference).
         """
-        LOGGER.info("=============== forward ================")
+        # LOGGER.info("=============== forward ================")
         if isinstance(x, dict):  # for cases of training and validating while training.
             return self.loss(x, *args, **kwargs)
         return self.predict(x, *args, **kwargs)
@@ -150,7 +150,7 @@ class BaseModel(torch.nn.Module):
         Returns:
             (torch.Tensor): The last output of the model.
         """
-        LOGGER.info("=============== predict ================")
+        # LOGGER.info("=============== predict ================")
         if augment:
             return self._predict_augment(x)
         return self._predict_once(x, profile, visualize, embed)
@@ -167,7 +167,7 @@ class BaseModel(torch.nn.Module):
         Returns:
             (torch.Tensor): The last output of the model.
         """
-        LOGGER.info("=============== _predict_once ================")
+        # LOGGER.info("=============== _predict_once ================")
         y, dt, embeddings = [], [], []  # outputs
         embed = frozenset(embed) if embed is not None else {-1}
         max_idx = max(embed)
@@ -323,7 +323,7 @@ class BaseModel(torch.nn.Module):
             batch (dict): Batch to compute loss on.
             preds (torch.Tensor | list[torch.Tensor], optional): Predictions.
         """
-        LOGGER.info("=============== loss ================")
+        # LOGGER.info("=============== loss ================")
         if getattr(self, "criterion", None) is None:
             self.criterion = self.init_criterion()
 
